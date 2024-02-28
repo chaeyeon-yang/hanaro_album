84c28f5d53908d108630d441e8d3648b7a94daf9
chaeyeon-yang <myday0827@naver.com>
2024-02-28 17:17:35 +0900

    🎉 feat: 프로젝트 초기설정

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./component/layout";
+
+function App() {
+    // const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}></Route>
+            </Routes>
+        </>
+    );
+}
+
+export default App;
---
+++ src/component/header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <div className="text-2xl">Hanaro Album</div>;
+};
+
+export default Header;
