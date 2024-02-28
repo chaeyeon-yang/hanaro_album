4969d8de8857e405f8905e77a060bbccf36f3ba7
chaeyeon-yang <myday0827@naver.com>
2024-02-28 18:37:28 +0900

    📦[chore]: #1 로그인 화면 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./component/layout";
+import LoginPage from "./page/loginPage";
 
 function App() {
     // const [count, setCount] = useState(0);
@@ -7,7 +8,9 @@ function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Layout />}></Route>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<LoginPage />}></Route>
+                </Route>
             </Routes>
         </>
     );
