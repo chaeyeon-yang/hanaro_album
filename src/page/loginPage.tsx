30ab147bc06620d991a9167836961a58565b22a2
chaeyeon-yang <myday0827@naver.com>
2024-02-29 14:32:29 +0900

    ✨[feat]: #2 사용자 정보 받아오기

--- src/page/loginPage.tsx
+++ src/page/loginPage.tsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { ValidateID } from "../util/validateID";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/mycontext";
+import axios from "axios";
 
 const LoginPage = () => {
     const navigate = useNavigate();
+    const context = useContext(AppContext);
 
     const [errorMsg, setErrorMsg] = useState<string>("");
     // TODO: context 변수로 변경해야함
     const [userId, setUserId] = useState<number>(0);
+
+    // TODO: 디바운스
     // 사용자 입력 받을 때 디바운스 적용하여
     // 과도한 이벤트 헨들러의 호출을 방지한다.
+
     const handleUserInput = (id: string) => {
         // 1. 사용자가 입력값을 모두 지운 경우 -> 경고 메세지 안보여주기
         // 2. 사용자가 유효하지 않은 값을 입력한 경우 -> 경고 메세지 보여주기
         // 3. 사용자가 유효한 값을 입력한 경우 -> 경고 메세지 안보여주기
+        // 4. 값을 입력하지 않은채 로그인 버튼을 누른 경우
+        // 유효한 값을 입력한 경우 - 페이지 이동
 
         if (ValidateID(id) || !id) {
             setErrorMsg("");
@@ -24,12 +32,24 @@ const LoginPage = () => {
     };
 
     const handleLogin = () => {
-        // 4. 값을 입력하지 않은채 로그인 버튼을 누른 경우
         if (!userId) {
             setErrorMsg("값을 입력해주세요.");
         } else {
-            // 유효한 값을 입력한 경우 - 페이지 이동
-            navigate("/albums");
+            const url = "https://jsonplaceholder.typicode.com/users/" + userId;
+            axios.get(url).then((res) => {
+                console.log(res.data);
+                context.dispatch({
+                    type: "LOGIN",
+                    value: {
+                        id: userId,
+                        name: res.data.name,
+                        username: res.data.username,
+                        isLogin: true,
+                    },
+                });
+
+                navigate("/albums");
+            });
         }
     };
 
