988670bef364cc836cc439bc3f6c78658aefc0fd
chaeyeon-yang <myday0827@naver.com>
2024-02-29 14:30:40 +0900

    ✨[feat]: #2 앨범리스트 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./component/layout";
-import LoginPage from "./page/LoginPage";
+import LoginPage from "./page/loginPage";
+import AlbumPage from "./page/albumPage";
+import { AppProvider } from "./context/mycontext";
 
 function App() {
     return (
-        <>
+        <AppProvider>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<LoginPage />}></Route>
+                    <Route path="/albums" element={<AlbumPage />}></Route>
                 </Route>
             </Routes>
-        </>
+        </AppProvider>
     );
 }
 
