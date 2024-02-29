812a496e2a3151fc561bfb1bb76ed4c6267d3b27
chaeyeon-yang <myday0827@naver.com>
2024-02-29 14:37:40 +0900

    💄[ui]: #2  앨범 리스트

---
+++ src/page/albumPage.tsx
@@ -0,0 +1,13 @@
+const AlbumPage = () => {
+    return (
+        <div className="flex mt-10 px-10">
+            <div className="flex items-center gap-4">
+                <div className="text-2xl font-semibold">앨범 목록</div>
+                <button className="bg-btnColor text-white font-bold py-2 px-3.5 rounded-lg cursor-pointer">
+                    앨범 상세보기
+                </button>
+            </div>
+        </div>
+    );
+};
+export default AlbumPage;
