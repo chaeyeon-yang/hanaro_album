dbdd04d06b1d8548ff7cb446193db2a32068e85e
chaeyeon-yang <myday0827@naver.com>
2024-02-29 14:31:32 +0900

    ✨[feat]: #2 로그인에 따른 헤더 유저 정보

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,7 +1,29 @@
+import { useContext } from "react";
+import { AppContext } from "../context/mycontext";
+
 const Header = () => {
+    const context = useContext(AppContext);
+
     return (
-        <div className="text-2xl mt-2 mx-2 py-4 px-6 font-semibold rounded-lg text-white bg-mainColor">
-            Hanaro Album
+        <div className="flex items-center justify-between w-full bg-mainColor rounded-lg px-6 py-5 mt-2">
+            <div className=" text-2xl font-semibold  text-white ">
+                Hanaro Album
+            </div>
+            {context.state.isLogin ? (
+                <div className="flex items-center">
+                    <div className="text-lg font-bold text-slate-600 mr-2">
+                        {context.state.id}
+                    </div>
+                    <div className="text-lg font-bold mr-4">
+                        {context.state.name}
+                    </div>
+                    <button className="bg-btnColor text-white font-bold px-3.5 py-2 rounded-lg cursor-pointer">
+                        Sign Out
+                    </button>
+                </div>
+            ) : (
+                <></>
+            )}
         </div>
     );
 };
--- src/component/layout.tsx
+++ src/component/layout.tsx
@@ -3,7 +3,7 @@ import Header from "./header";
 
 const Layout = () => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col mx-2">
             <Header />
             <Outlet />
         </div>
