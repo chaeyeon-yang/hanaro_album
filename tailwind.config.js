1b31e268dcc5b862ce1289bb19fa199e9bf59e2a
chaeyeon-yang <myday0827@naver.com>
2024-03-01 20:21:17 +0900

    🎨[style]: #9 에러 컬러 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ export default {
             colors: {
                 mainColor: "#2B8AC1",
                 btnColor: "#3AC11B",
+                errorColor: "#F4800B",
+                errorSubColor: "#FCF0E5",
             },
             backgroundImage: {
                 loginImage: "url(./public/person.svg)",
