fea29aacd02103ad86beafb73153d3f475fa3ccf
chaeyeon-yang <myday0827@naver.com>
2024-03-01 15:39:49 +0900

    🎨[style]: #6 localStorage 값 뿌려주기

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,9 +1,11 @@
 import { useContext } from "react";
 import { AppContext } from "../context/mycontext";
 import { useNavigate } from "react-router-dom";
+import { getStateFromLocalStorage } from "../util/localStorage";
 
 const Header = () => {
     const context = useContext(AppContext);
+    const userInfo = getStateFromLocalStorage("loginState");
     const navigate = useNavigate();
     const handleLogout = () => {
         context.dispatch({
@@ -23,13 +25,13 @@ const Header = () => {
             <div className=" text-2xl font-semibold  text-white ">
                 Hanaro Album
             </div>
-            {context.state.isLogin ? (
+            {userInfo.isLogin ? (
                 <div className="flex items-center">
                     <div className="text-lg font-bold text-slate-600 mr-2">
-                        {context.state.id}
+                        {userInfo.id}
                     </div>
                     <div className="text-lg font-bold mr-4">
-                        {context.state.name}
+                        {userInfo.name}
                     </div>
                     <button
                         className="bg-btnColor text-white font-bold px-3.5 py-2 rounded-lg cursor-pointer"
