e070518af2a8eb0b0cbf973fbe76aa443818a15e
chaeyeon-yang <myday0827@naver.com>
2024-03-01 19:58:46 +0900

    ✨[feat]: #8 선택된 앨범정보 저장하는 reducer

--- src/context/albumReducer.tsx
+++ src/context/albumReducer.tsx
@@ -1,18 +1,32 @@
 import { TAlbumAction, TAlbum } from "../type/album/albumInfo";
-import { saveStateToLocalStorage } from "../util/localStorage";
+import {
+    getStateFromLocalStorage,
+    saveStateToLocalStorage,
+} from "../util/localStorage";
 
+const selectedAlbumState = getStateFromLocalStorage("selectedAlbumState");
+export const initialSelectedAlbumState: TAlbum = selectedAlbumState;
 export const initialAlbumState: TAlbum[] = [];
+
+export const AlbumDetailReducer = (
+    state = initialSelectedAlbumState,
+    action: TAlbumAction
+): TAlbum => {
+    switch (action.type) {
+        case "SELECTED_ALBUM": {
+            saveStateToLocalStorage("selectedAlbumState", action.value);
+            return action.value;
+        }
+        default:
+            return state;
+    }
+};
 
 export const AlbumReducer = (
     state = initialAlbumState,
     action: TAlbumAction
 ): TAlbum[] => {
     switch (action.type) {
-        case "SELECTED_ALBUM": {
-            const newState = [...state, action.value];
-            saveStateToLocalStorage("selectedAlbumState", newState);
-            return newState;
-        }
         case "ALBUM_LIST": {
             const newState = [...action.value];
             saveStateToLocalStorage("albumListState", newState);
